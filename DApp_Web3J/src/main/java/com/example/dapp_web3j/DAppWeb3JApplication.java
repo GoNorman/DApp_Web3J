75cc08834a825299b1b01220ff61ff60e8072075
GoNorman <dr.cheif-afanasyev@yandex.ru>
2023-10-19 19:50:32 +0200

    Feat : Add few features

--- DApp_Web3J/src/main/java/com/example/dapp_web3j/DAppWeb3JApplication.java
+++ DApp_Web3J/src/main/java/com/example/dapp_web3j/DAppWeb3JApplication.java
@@ -3,6 +3,7 @@ package com.example.dapp_web3j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.web3j.protocol.Web3j;
+import org.web3j.crypto.Credentials;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.methods.response.EthBlockNumber;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
@@ -17,12 +18,16 @@ import java.util.concurrent.ExecutionException;
 @SpringBootApplication
 public class DAppWeb3JApplication {
 
-    Web3j web3j = Web3j.build(new HttpService("localhost:8545"));
+    Web3j web3j = Web3j.build(new HttpService("https://mainnet.infura.io/v3/d21e9050c0a74e80987c0813c07d0558"));
 
+    Credentials credentials = Credentials.create("cb4dad1857a5b618c3136bc270a689db618a6f04d4e05e9325a110eb8061a90d");
+
+    //d21e9050c0a74e80987c0813c07d0558
     public static final String DEFAULT_ADDRESS = "0x281055afc982d96fab65b3a49cac8b878184cb16";
     public static void main(String[] args) {
 
         SpringApplication.run(DAppWeb3JApplication.class, args);
+
     }
 
     public EthBlockNumber getBlockNumber() throws ExecutionException, InterruptedException {
@@ -45,7 +50,5 @@ public class DAppWeb3JApplication {
                 sendAsync().get();
         return result;
     }
-
-
 
 }
