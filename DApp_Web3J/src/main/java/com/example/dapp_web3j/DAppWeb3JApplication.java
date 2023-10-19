95409ef5fc4df0d524b79e458e0dc1e0185a1c1a
GoNorman <dr.cheif-afanasyev@yandex.ru>
2023-10-19 18:50:39 +0200

    feat : add getters from contract and compile contract

--- DApp_Web3J/src/main/java/com/example/dapp_web3j/DAppWeb3JApplication.java
+++ DApp_Web3J/src/main/java/com/example/dapp_web3j/DAppWeb3JApplication.java
@@ -2,12 +2,50 @@ package com.example.dapp_web3j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.response.EthBlockNumber;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.http.HttpService;
+
+
+import java.util.concurrent.ExecutionException;
+
+
 
 @SpringBootApplication
 public class DAppWeb3JApplication {
 
+    Web3j web3j = Web3j.build(new HttpService("localhost:8545"));
+
+    public static final String DEFAULT_ADDRESS = "0x281055afc982d96fab65b3a49cac8b878184cb16";
     public static void main(String[] args) {
+
         SpringApplication.run(DAppWeb3JApplication.class, args);
     }
+
+    public EthBlockNumber getBlockNumber() throws ExecutionException, InterruptedException {
+        EthBlockNumber result = new EthBlockNumber();
+        result = this.web3j.ethBlockNumber().sendAsync().get();
+
+        return result;
+    }
+
+    public EthGetTransactionCount getTransactionCount() throws ExecutionException, InterruptedException {
+        EthGetTransactionCount result = new EthGetTransactionCount();
+        result = this.web3j.ethGetTransactionCount(DEFAULT_ADDRESS, DefaultBlockParameter.valueOf("latest")).
+                sendAsync().get();
+        return result;
+    }
+
+    public EthGetBalance getEthBalance() throws  ExecutionException, InterruptedException {
+        EthGetBalance result = new EthGetBalance();
+        result = this.web3j.ethGetBalance(DEFAULT_ADDRESS, DefaultBlockParameter.valueOf("letest")).
+                sendAsync().get();
+        return result;
+    }
+
+
 
 }
